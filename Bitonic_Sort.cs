3a9eac451cc87f637c03a380a7d6f1d688e44d48
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-04-24 14:36:28 +0430

    Create Bitonic_Sort.cs

---
+++ Bitonic_Sort.cs
@@ -0,0 +1,91 @@
+using System;
+enum Direction
+{
+	descending,
+	ascending
+}
+class BitonicSort
+{
+
+
+	static void Swap<T>(ref T lhs, ref T rhs)
+	{
+		T temp;
+		temp = lhs;
+		lhs = rhs;
+		rhs = temp;
+	}
+
+	public static void compAndSwap(int[] a, int i, int j, Direction dir)
+	{
+		if ((a[i] > a[j]))
+        {
+			if (dir == Direction.ascending)
+			{
+				Swap(ref a[i], ref a[j]);
+			}
+		}
+		else if(dir == Direction.descending)
+        {
+			Swap(ref a[i], ref a[j]);
+		}
+			
+	}
+	public static void bitonicMerge(int[] a, int low, int count, Direction dir)
+	{
+		if (count > 1)
+		{
+			int k = count / 2;
+			for (int i = low; i < low + k; i++)
+				compAndSwap(a, i, i + k, dir);
+        
+			bitonicMerge(a, low, k, dir);
+			bitonicMerge(a, low + k, k, dir);
+		}
+	}
+	public static void bitonicSort(int[] a, int low, int count, Direction dir)
+	{
+		if (count > 1)
+		{
+			int k = count / 2;
+
+			bitonicSort(a, low, k, Direction.ascending);
+
+			bitonicSort(a, low + k, k, Direction.descending);
+
+			bitonicMerge(a, low, count, dir);
+		}
+	}
+
+	public static void sort(int[] a, int count, Direction up)
+	{
+		bitonicSort(a, 0, count, up);
+	}
+	static void Main()
+	{
+		var watch = new System.Diagnostics.Stopwatch();
+
+		int[] a = new int[(int)Math.Pow(2,6)];
+		int arrlen = a.Length;
+		Random rd = new Random();
+
+		for (int i = 0; i < arrlen; i++)
+			a[i] = rd.Next(1, 1000);
+
+		Console.Write("UnSorted array: \n");
+		for (int i = 0; i < arrlen; i++)
+			Console.Write(a[i] + " ");
+
+		watch.Start();
+		sort(a, a.Length , Direction.ascending);
+		watch.Stop();
+
+		Console.Write("\n Sorted array: \n");
+		for (int i = 0; i < arrlen; i++)
+			Console.Write(a[i] + " ");
+
+		Console.WriteLine("");
+		Console.WriteLine($"Execution Time: {watch.Elapsed} ");
+		Console.ReadLine();
+	}
+}
